57f6df99941a865ee916b316bad666f4701f0370
Vasjan Çupri <vasjancupri04@gmail.com>
2022-09-30 15:24:53 +0200

    Added some robots and removed the old stuff

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = ({ name, email, id }) => {
+  return (
+    <div
+      className="tc near-white
+    bg-light-purple dib br3 pa3 ma2 grow bw2 shadow-5"
+    >
+      <img alt="robot" src={` https://robohash.org/${id}?size=200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
