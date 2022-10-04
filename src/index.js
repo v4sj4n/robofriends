477156cb6395bbafd28ff19bf479e189ca62e053
Vasjan Çupri <vasjancupri04@gmail.com>
2022-10-04 16:38:13 +0200

    made some changes to compile in netlify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Card from "./Card";
 import Cardlist from "./Cardlist";
 import "tachyons";
 import { robots } from "./robots";
