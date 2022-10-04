f793b36c453b0a306dcfd12cb6002c770b0166ea
Vasjan Çupri <vasjancupri04@gmail.com>
2022-10-04 18:35:47 +0200

    Used a link in a JSON format to import the robots

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import Cardlist from "./Cardlist";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
@@ -21,13 +25,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Cardlist robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Cardlist robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 export default App;
