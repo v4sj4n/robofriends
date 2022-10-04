60034639fdaa54d159b6941f7307a83e3da644e3
Vasjan Çupri <vasjancupri04@gmail.com>
2022-10-04 16:04:08 +0200

    Added all the robots and fixed some issues

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Card from "./Card";
+import Cardlist from "./Cardlist";
 import "tachyons";
 import { robots } from "./robots";
 import reportWebVitals from "./reportWebVitals";
@@ -9,11 +10,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </div>
+    <Cardlist robots={robots} />
   </React.StrictMode>
 );
 
